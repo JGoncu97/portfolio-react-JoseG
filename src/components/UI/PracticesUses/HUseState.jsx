26144a3841e458d07c99cd94dfcc55f23310fc7f
JGoncu97 <jggc1197@gmail.com>
2024-11-05 10:18:47 -0500

    Ejercicio Calculadora

--- src/components/UI/PracticesUses/HUseState.jsx
+++ src/components/UI/PracticesUses/HUseState.jsx
@@ -2,27 +2,105 @@
 import React, { useState } from 'react';
 
 export const HUseState = () => {
-    const [input, setInputName] = useState('');
-    const [nombre, setNombre] = useState('');
+   
+    // /*Ejercicio Calculadora*/ 
+    //const [input, setInput1] = useState('');
+    // const [input2, setInput2] = useState('');
+    // const [resultado, setResultado] = useState('');
 
-    const handleNameChange = (e) => {
-        setInputName(e.target.value);
-    };
+    // const handleCalculate = (e) => {
+    //     setInput1(e.target.value);
+       
+    // };
+ 
+    // const handleCalculate2 = (e) => {
+    //     setInput2(e.target.value);
+       
+    // };
 
-    const enviarNombre = () => {
-        setNombre(input); 
-    };
+  
+
+    // const sendResultadoSuma = () => {
+    
+    //     const num1 = parseFloat(input);
+    //     const num2 = parseFloat(input2);
+        
+    //     if(!isNaN(num1) && !isNaN(num2)){
+    //         setResultado(num1 + num2); 
+    //     }else{
+    //         setResultado("No se permiten datos vacios")
+            
+    //     }
+
+    // }
+
+    // const sendResultadoResta = () => {
+    
+    //     const num1 = parseFloat(input);
+    //     const num2 = parseFloat(input2);
+        
+    //     if(!isNaN(num1) && !isNaN(num2)){
+    //         setResultado(num1 - num2); 
+    //     }else{
+    //         setResultado("No se permiten datos vacios")
+            
+    //     }
+
+    // }
+
+    // const sendResultadoMultiplicar = () => {
+    
+    //     const num1 = parseFloat(input);
+    //     const num2 = parseFloat(input2);
+        
+    //     if(!isNaN(num1) && !isNaN(num2)){
+    //         setResultado(num1 * num2); 
+    //     }else{
+    //         setResultado("No se permiten datos vacios")
+            
+    //     }
+    // }
+
+    // const sendResultadoDividir = () => {
+    
+    //     const num1 = parseFloat(input);
+    //     const num2 = parseFloat(input2);
+
+    //     if(!isNaN(num1) && !isNaN(num2)){
+    //         setResultado(num1 / num2); 
+    //     }else{
+    //         setResultado("No se permiten datos vacios")
+            
+    //     }
+     
+            
+       
+
+    // }
+    
+    const buttonStyle = 'border-2 border-solid border-black ml-2 p-1.5 rounded-md bg-red-600'
+
 
     return (
         <>
-            <div>
-                <h2>Bienvenido {nombre}</h2> 
+            {/* <div>
+                <h2>Calculadora </h2> 
                 <input 
-                    placeholder='tu nombre es?' 
-                    onChange={handleNameChange}
+                    placeholder='Numero 1' 
+                    onChange={handleCalculate}
                 />
-                <button onClick={enviarNombre}>Imprimir</button> 
-            </div>
+                <input 
+                    placeholder='Numero 2' 
+                    onChange={handleCalculate2}
+                />
+                <button className={buttonStyle} onClick={sendResultadoSuma}>Sumar</button> 
+                <button className={buttonStyle} onClick= {sendResultadoResta}>Restar</button> 
+                <button className={buttonStyle} onClick={sendResultadoMultiplicar}>Multiplicar</button> 
+                <button className={buttonStyle} onClick={sendResultadoDividir}>Dividir</button> 
+                <h3>{resultado}</h3>
+            </div> */}
+
+            <h1>Hola world</h1>
         </>
     );
 };
